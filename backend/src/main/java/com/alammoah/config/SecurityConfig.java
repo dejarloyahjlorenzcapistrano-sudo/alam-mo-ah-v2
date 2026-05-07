293709d6cfd5e208198c7514be3b017627b2e6c7
dejarloyahjlorenzcapistrano-sudo <dejarlo.yahjlorenz.capistrano@gmail.com>
2026-05-07 21:59:45 +0800

    403 fix

--- backend/src/main/java/com/alammoah/config/SecurityConfig.java
+++ backend/src/main/java/com/alammoah/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -52,6 +53,7 @@ public class SecurityConfig {
                 )
 
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
